8f9aab4466ca17bd9bb746c27bc2ce3f8dce2d38
Accdia <accidia12139@outlook.com>
2023-02-21 20:35:33 +0800

    hint

---
+++ Week2/Key_Detection/SmartWear/Core/Src/gpio.c
@@ -0,0 +1,78 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LED2_Pin|LED3_Pin|LED1_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = KEY_UP_Pin;         /* 引脚 */
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;   /* 输入模式 */
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;     /* 带下拉 */
+  HAL_GPIO_Init(KEY_UP_GPIO_Port, &GPIO_InitStruct);    /* 初始化 */
+
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = KEY2_Pin;           /* 引脚 */
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;   /* 输入模式 */
+  GPIO_InitStruct.Pull = GPIO_PULLUP;       /* 带上拉 */
+  HAL_GPIO_Init(KEY2_GPIO_Port, &GPIO_InitStruct);      /* 初始化 */
+
+  /*Configure GPIO pins : PBPin PBPin PBPin */
+  GPIO_InitStruct.Pin = LED2_Pin|LED3_Pin|LED1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
---
+++ Week2/Key_Detection/SmartWear/MDK-ARM/SmartWear/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+smartwear\startup_stm32f103xb.o: startup_stm32f103xb.s
