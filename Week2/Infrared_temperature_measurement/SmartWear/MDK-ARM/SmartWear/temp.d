f98e7ba7043b387ffe3364b9c4d49b96fac34c52
Accdia <accidia12139@outlook.com>
2023-02-22 15:27:39 +0800

    hint

--- Week2/Infrared_temperature_measurement/SmartWear/MDK-ARM/SmartWear/temp.d
+++ Week2/Infrared_temperature_measurement/SmartWear/MDK-ARM/SmartWear/temp.d
@@ -8,14 +8,14 @@ smartwear\temp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
 smartwear\temp.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
 smartwear\temp.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
 smartwear\temp.o: ../Drivers/CMSIS/Include/core_cm3.h
-smartwear\temp.o: D:\MDK5\ARM\ARMCC\Bin\..\include\stdint.h
+smartwear\temp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 smartwear\temp.o: ../Drivers/CMSIS/Include/cmsis_version.h
 smartwear\temp.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
 smartwear\temp.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
 smartwear\temp.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
 smartwear\temp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
 smartwear\temp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-smartwear\temp.o: D:\MDK5\ARM\ARMCC\Bin\..\include\stddef.h
+smartwear\temp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
 smartwear\temp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
 smartwear\temp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
 smartwear\temp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
@@ -28,4 +28,4 @@ smartwear\temp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
 smartwear\temp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
 smartwear\temp.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
 smartwear\temp.o: ../Core/Inc/usart.h
-smartwear\temp.o: D:\MDK5\ARM\ARMCC\Bin\..\include\stdio.h
+smartwear\temp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
--- Week2/Serial_transceiver/SmartWear/MDK-ARM/SmartWear/led.d
+++ Week2/Serial_transceiver/SmartWear/MDK-ARM/SmartWear/led.d
@@ -8,14 +8,14 @@ smartwear\led.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
 smartwear\led.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
 smartwear\led.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
 smartwear\led.o: ../Drivers/CMSIS/Include/core_cm3.h
-smartwear\led.o: D:\MDK5\ARM\ARMCC\Bin\..\include\stdint.h
+smartwear\led.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 smartwear\led.o: ../Drivers/CMSIS/Include/cmsis_version.h
 smartwear\led.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
 smartwear\led.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
 smartwear\led.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
 smartwear\led.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
 smartwear\led.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-smartwear\led.o: D:\MDK5\ARM\ARMCC\Bin\..\include\stddef.h
+smartwear\led.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
 smartwear\led.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
 smartwear\led.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
 smartwear\led.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
